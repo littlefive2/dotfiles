0b019e47b1c32579f6521a61dfd7c80a7c9f408d
yusuke furukawa <littlefive.jp@gmail.com>
2014-12-07 04:53:16 +0900

    add echo for what doing

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # bash
+echo "bashfile settigns....."
 for file in bash_profile bashrc bashrc_mininum
 do
     FILE=$HOME/.$file
@@ -12,6 +13,7 @@ done
 source $HOME/.bashrc
 
 #vim
+echo "vimrcfile settigns....."
 for file in vimrc vimrc.basic vimrc.bundle vimrc.color vimrc.comment vimrc.statusline
 do
     FILE=$HOME/.$file
@@ -21,6 +23,8 @@ do
     ln -s $HOME/dotfiles/vimrc/$file $HOME/.$file
 done
 
+echo "new create vim.secret"
+for file in vimrc vimrc.basic vimrc.bundle vimrc.color vimrc.comment vimrc.statusline
 touch $HOME/.vim.secret 
 chmod +x $HOME/.vim.secret 
 $HOME/.vim.secret << EOF
@@ -28,5 +32,6 @@ let g:SimplenoteUsername =
 let g:SimplenotePassword = 
 let g:SimplenoteNoteFormat="[%D] %N%>%T"
 EOF
+echo "*Please input password for Simplenote"
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
