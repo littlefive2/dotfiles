6dbddf8a9011efef527ea8ee73ae5abf01e55145
yusuke furukawa <littlefive.jp@gmail.com>
2014-12-07 04:47:06 +0900

    new create initial script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-find -type f -not -iname "setup.sh" | xargs -i cp {} $HOME/.{}
+# bash
+for file in bash_profile bashrc bashrc_mininum
+do
+    FILE=$HOME/.$file
+    if [ -a $FILE ]; then
+        rm $FILE
+    fi
+    ln -s $HOME/dotfiles/bash/$file $HOME/.$file
+done
+source $HOME/.bashrc
+
+#vim
+for file in vimrc vimrc.basic vimrc.bundle vimrc.color vimrc.comment vimrc.statusline
+do
+    FILE=$HOME/.$file
+    if [ -a $FILE ]; then
+        rm $FILE
+    fi
+    ln -s $HOME/dotfiles/vimrc/$file $HOME/.$file
+done
+
+touch $HOME/.vim.secret 
+chmod +x $HOME/.vim.secret 
+$HOME/.vim.secret << EOF
+let g:SimplenoteUsername = 
+let g:SimplenotePassword = 
+let g:SimplenoteNoteFormat="[%D] %N%>%T"
+EOF
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
