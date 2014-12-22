7d875bf03539d8ed683f0ed214b590405b19c1d0
yusuke furukawa <littlefive.jp@gmail.com>
2014-12-22 11:53:55 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
+
+git clone https://github.com/frkwy/dotfiles.git $HOME/dotfiles
+
 # bash
 echo "bashfile settigns....."
 for file in bash_profile bashrc bashrc_mininum
 do
-    FILE=/home/vagrant/.$file
+    FILE=$HOME/.$file
     if [ -a $FILE ]; then
         rm $FILE
     fi
-    ln -s /home/vagrant/dotfiles/bash/$file /home/vagrant/.$file
+    ln -s $HOME/dotfiles/bash/$file $HOME/.$file
 done
 
 #vim
 echo "vimrcfile settigns....."
 for file in vimrc vimrc.basic vimrc.bundle vimrc.color vimrc.comment vimrc.statusline
 do
-    FILE=/home/vagrant/.$file
+    FILE=$HOME/.$file
     if [ -a $FILE ]; then
         rm $FILE
     fi
-    ln -s /home/vagrant/dotfiles/vimrc/$file /home/vagrant/.$file
+    ln -s $HOME/dotfiles/vimrc/$file $HOME/.$file
 done
 
 echo "new create vim.secret"
-touch /home/vagrant/.vim.secret 
-chmod +x /home/vagrant/.vim.secret 
-/home/vagrant/.vim.secret << EOF
+touch $HOME/.vim.secret 
+chmod +x $HOME/.vim.secret 
+$HOME/.vim.secret << EOF
 let g:SimplenoteUsername = 
 let g:SimplenotePassword = 
 let g:SimplenoteNoteFormat="[%D] %N%>%T"
 EOF
 echo "*Please input password for Simplenote"
 
-git clone https://github.com/gmarik/Vundle.vim.git /home/vagrant/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
 
 echo "tmux settings....."
-ln -s /home/vagrant/dotfiles/tmux/tmux_ubuntu.conf /home/vagrant/.tmux.conf
+ln -s $HOME/dotfiles/tmux/tmux_ubuntu.conf $HOME/.tmux.conf
 
