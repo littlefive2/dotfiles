2ca94902cb7d8f8998af900acd43bc4a28c4b649
yusuke furukawa <littlefive.jp@gmail.com>
2015-10-07 20:32:45 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+# clone my rcfiles
 git clone https://github.com/frkwy/dotfiles.git $HOME/rcfiles
 
-############################## 
-#bash
-############################## 
-echo "bashfile settigns....."
+# create dir
 mkdir ~/tmp
 mkdir ~/bin
 mkdir ~/Memo
 
+# put file
 mv $HOME/rcfiles/git-prompt.sh $HOME/bin/git-prompt.sh
 mv $HOME/rcfiles/git-completion.bash $HOME/bin/git-completion.bash
 mv $HOME/rcfiles/cdd $HOME/bin/cdd
 
+# put bash file
 for file in bash_profile bashrc bashrc_mininum
 do
     FILE=$HOME/.$file
@@ -23,10 +23,7 @@ do
     ln -sf $HOME/rcfiles/$file $HOME/.$file
 done
 
-############################## 
-#vim
-############################## 
-echo "vimrcfile settigns....."
+#put vimrc file
 for file in vimrc vimrc.basic vimrc.bundle vimrc.color vimrc.comment vimrc.statusline
 do
     FILE=$HOME/.$file
@@ -36,6 +33,7 @@ do
     ln -sf $HOME/rcfiles/$file $HOME/.$file
 done
 
+# Simplenote
 echo "new create vim.secret"
 touch $HOME/.vim.secret 
 chmod +x $HOME/.vim.secret 
@@ -46,13 +44,15 @@ let g:SimplenoteNoteFormat="[%D] %N%>%T"
 EOF
 echo "*Please input password for Simplenote"
 
+# vundle settings
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
 
+# tmux
 git clone https://github.com/tmux-plugins/tmux-yank ~/rcfiles/tmux-yank
-
 echo "tmux settings....."
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 
-sed s/\"// $HOME/.vimrc.color  > $HOME/.vimrc.color
-#source ~/.bash_profile
+
+sed s/\"// $HOME/rcfiles/vimrc.color > $HOME/.vimrc.color
+source ~/.bash_profile
