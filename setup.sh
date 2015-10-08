df8985a5a046156079265e35c368f88883297698
yusuke furukawa <littlefive.jp@gmail.com>
2015-10-09 01:45:10 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# locale
+mkdir $HOME/.config
+echo "
+LANG=ja_JP.UTF-8
+LC_CTYPE=ja_JP.UTF-8
+LC_NUMERIC=ja_JP.UTF-8
+LC_TIME=ja_JP.UTF-8
+LC_COLLATE=ja_JP.UTF-8
+LC_MONETARY=ja_JP.UTF-8
+LC_MESSAGES=ja_JP.UTF-8
+LC_PAPER="ja_JP.UTF-8"
+LC_NAME="ja_JP.UTF-8"
+LC_ADDRESS="ja_JP.UTF-8"
+LC_TELEPHONE="ja_JP.UTF-8"
+LC_MEASUREMENT="ja_JP.UTF-8"
+LC_IDENTIFICATION="ja_JP.UTF-8"
+LC_ALL=
+" > $HOME/.config/locale.conf
+
 # clone my rcfiles
 git clone https://github.com/frkwy/dotfiles.git $HOME/rcfiles
 
